1cd0442e8145ecbb36fb4bb73f181ae47d4290a9
Josemaa13 <josemariaggu13@gmail.com>
2026-03-09 23:01:22 +0100

    mock

--- TestStubInvoice.py
+++ TestStubInvoice.py
@@ -23,3 +23,35 @@ class TestInvoiceWithStubs(unittest.TestCase):
         # 4. Verificación:
         # 100.00 + 50.50 = 150.50
         self.assertEqual(inv.total, Decimal("150.50"))
+    
+    def test_kilos_to_bill(self):
+        inv = Invoice(number="INV-003", date=date.today(), currency="USD")
+        
+        line_stub3 = MagicMock()
+        line_stub3.kilos_to_bill.return_value = Decimal("3.55")
+        
+        line_stub4 = MagicMock()
+        line_stub4.kilos_to_bill.return_value = Decimal("1.35")
+        
+        inv.add_line(line_stub3)
+        inv.add_line(line_stub4)
+        
+        self.assertEqual(inv.kilos_to_bill(), Decimal("4.90"))
+        
+    def test_unit_price(self):
+        inv = Invoice(number="INV-003", date=date.today(), currency="USD")
+        
+        line_stub5 = MagicMock()
+        line_stub5.kilos_to_bill.return_value = Decimal("1")
+        line_stub5.unit_price.return_value = Decimal("2.0")
+        
+        line_stub6 = MagicMock()
+        line_stub6.kilos_to_bill.return_value = Decimal("3.0")
+        line_stub6.unit_price.return_value = Decimal("5.0")
+        
+        inv.add_line(line_stub5)
+        inv.add_line(line_stub6)
+        
+        self.assertEqual(inv.unit_price(), Decimal("4.25"))
+        
+
